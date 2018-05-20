966dffcb4b9ee1c400ba6583b48b0b0b5e6113b9
JimmyLIn <linyuan5856139@163.com>
2018-05-20 21:16:28 +0800

    update

---
+++ RayTracy/Assets/Script/CreatPNG.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class CreatPNG : MonoBehaviour
+{
+    public const int WIDTH = 800;
+    public const int HEIGHT = 600;
+    public const string PATH="Assets/Texture/Target.jpg";
+
+
+    private static Color[] MakeTexture()
+    {
+        int total = WIDTH  * HEIGHT;
+        Color[] colors = new Color[total];
+
+        for (int j = HEIGHT - 1; j >= 0; j--)
+        {
+            for (int i = 0; i < WIDTH ; i++)
+            {
+                colors[i + j * WIDTH ] = new Color(i / (float) WIDTH , j / (float) HEIGHT, 0.2f);
+            }
+        }
+
+        return colors;
+    }
+
+    public static void Creat_FileInternal(Color[]colors)
+    {
+        Texture2D tex = new Texture2D(WIDTH, HEIGHT, TextureFormat.RGBA32, false);
+
+        tex.SetPixels(colors);
+        tex.Apply();
+
+        byte[] bytes = tex.EncodeToJPG();
+
+        using (FileStream fs = new FileStream(PATH, FileMode.Create))
+        {
+            using (BinaryWriter bw = new BinaryWriter(fs))
+            {
+                bw.Write(bytes);
+            }
+        }
+
+
+        Debug.LogWarning("Creat PNG Done");
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("RayTrace/CreatTexture")]
+    public static void CreatFile()
+    {
+        Creat_FileInternal(MakeTexture());
+    }
+    
+}
---
+++ RayTracy/Assets/Script/Ray.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray
+{
+
+	private Vector3 origin;
+	private Vector3 direction;
+	private float t;
+	
+	public Ray(Vector3 o,Vector3 d)
+	{
+		this.origin = o;
+		this.direction = d;
+	}
+
+	public Vector3 GetPoint(float t)
+	{
+		return this.origin + this.direction * t;
+	}
+}
